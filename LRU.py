dff57669572839fdc1554be0e78e26999b08c0e0
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-04-16 23:31:13 +0530

    Added methods to LRU

--- LRU.py
+++ LRU.py
@@ -2,4 +2,9 @@ class LRUCache:
     def __init__(self,size):
         self.lrucache = []
         self.size = 6
-    
+    def put(self,item):
+        pass
+    def get(self):
+        pass
+    def get_cache(self):
+        pass
